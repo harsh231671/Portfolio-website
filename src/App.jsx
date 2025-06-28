16c70edb4206d65971cfc228610c0c1d5df72995
harsh231671 <115443950+harsh231671@users.noreply.github.com>
2025-06-28 18:16:42 +0530

    project created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="heading">
+        <h1 className="main-heading">Hello!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
